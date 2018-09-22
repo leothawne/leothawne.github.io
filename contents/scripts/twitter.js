c0a66f4ec0e692ac457eff49eab1e83973b15c88
Leonardo Thawne <31146142+leothawne@users.noreply.github.com>
2018-09-22 19:39:59 -0300

    Update twitter.js

--- contents/scripts/twitter.js
+++ contents/scripts/twitter.js
@@ -1,23 +1,20 @@
 $(document).ready(function (){
-	if(tweetEnabled == true){
-		$.getJSON("https://publish.twitter.com/oembed?url=" + tweetURL, function (responseData){
-			var fixedTweet = "";
-			$.each(responseData, function (url, author_name, author_url, html, width, height, type, cache_age, provider_name, provider_url, version){
-				fixedTweet = "URL: " + url + "<br /><br />" +
-					"Author Name: " + author_name + "<br /><br />" +
-					"Author URL: " + author_url + "<br /><br />" +
-					"HTML: " + html + "<br /><br />" +
-					"Width: " + width + "<br /><br />" +
-					"Height: " + height + "<br /><br />" +
-					"Type: " + type + "<br /><br />" +
-					"Cache Age: " + cache_age + "<br /><br />" +
-					"Provider Name: " + provider_name + "<br /><br />" +
-					"Provider URL: " + provider_url + "<br /><br />" +
-					"Version: " + version;
-			});
-			$(".fixedTweet").html(fixedTweet);
+	if(newsEnabled == true){
+		$.getJSON("/contents/json/news.json", function (responseData){
+			var	fixedNews = "URL: " + url + "<br /><br />" +
+				"Author Name: " + author_name + "<br /><br />" +
+				"Author URL: " + author_url + "<br /><br />" +
+				"HTML: " + html + "<br /><br />" +
+				"Width: " + width + "<br /><br />" +
+				"Height: " + height + "<br /><br />" +
+				"Type: " + type + "<br /><br />" +
+				"Cache Age: " + cache_age + "<br /><br />" +
+				"Provider Name: " + provider_name + "<br /><br />" +
+				"Provider URL: " + provider_url + "<br /><br />" +
+				"Version: " + version;
+			$(".fixedNews").html(fixedNews);
 		});
 	} else {
-		$(".fixedTweet").html("Twitter API disabled by the administrator!");
+		$(".fixedNews").html("News disabled by the administrator!");
 	}
 });
