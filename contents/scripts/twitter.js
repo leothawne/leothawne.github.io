1323846cee29ed8a6ddc0a126a038729967f2d94
Leonardo Thawne <31146142+leothawne@users.noreply.github.com>
2018-09-22 18:59:17 -0300

    Update twitter.js

--- contents/scripts/twitter.js
+++ contents/scripts/twitter.js
@@ -1,11 +1,21 @@
 $(document).ready(function (){
 	if(tweetEnabled == true){
 		$.getJSON("https://publish.twitter.com/oembed?url=" + tweetURL, function (responseData){
-			var items = [];
+			var fixedTweet = "";
 			$.each(responseData, function (url, author_name, author_url, html, width, height, type, cache_age, provider_name, provider_url, version){
-				items.push("");
-				//continuar
+				fixedTweet = "URL: " + url + "<br /><br />" +
+					"Author Name: " + author_name + "<br /><br />" +
+					"Author URL: " + author_url + "<br /><br />" +
+					"HTML: " + html + "<br /><br />" +
+					"Width: " + width + "<br /><br />" +
+					"Height: " + height + "<br /><br />" +
+					"Type: " + type + "<br /><br />" +
+					"Cache Age: " + cache_age + "<br /><br />" +
+					"Provider Name: " + provider_name + "<br /><br />" +
+					"Provider URL: " + provider_url + "<br /><br />" +
+					"Version: " + version;
 			});
+			$(".fixedTweet").html(fixedTweet);
 		});
 	} else {
 		$(".fixedTweet").html("Twitter API disabled by the administrator!");
